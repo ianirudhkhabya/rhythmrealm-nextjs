d2fdf99c84c6d59c9df62d8e71b9865a37713ba7
ianirudhkhabya <ianirudhkhabya@gmail.com>
2024-01-12 12:45:02 +0530

    Environment Setup

---
+++ app/(site)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="text-green-500 flex items-center justify-center h-screen">
+      Hello Spotify
+    </div>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import "./globals.css";
+
+const font = Figtree({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Spotify Clone",
+  description: "Listen to your favorite music for free",
+  icons: {
+    icon: [
+      { url: "/meta/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/meta/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: {
+      url: "/meta/apple-touch-icon.png",
+      sizes: "180x180",
+      type: "image/png",
+    },
+  },
+  metadataBase: new URL("https://localhost:3000/"),
+  openGraph: {
+    url: "https://localhost:3000/",
+    title: "Spotify Clone",
+    description: "Listen to your favorite music for free",
+    type: "website",
+  },
+  manifest: "/meta/site.webmanifest",
+  keywords: ["Spotify", "Clone", "Music", "Free", "ianirudhkhabya"],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
