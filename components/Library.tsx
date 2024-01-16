02adf38d8fc5ffaa34f2a94b18a44d0177ecca1f
ianirudhkhabya <ianirudhkhabya@gmail.com>
2024-01-16 18:03:28 +0530

    Add authentication and upload modals to Library component

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,9 +1,23 @@
 "use client";
 
+import useAuthModal from "@/hooks/useAuthModal";
+import useUploadModal from "@/hooks/useUploadModal";
+import { useUser } from "@/hooks/useUser";
 import { AiOutlinePlus } from "react-icons/ai";
 import { MdLibraryMusic } from "react-icons/md";
 
 const Library = () => {
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const { user } = useUser();
+
+  const onClick = () => {
+    if (!user) {
+      authModal.onOpen();
+    }
+    return uploadModal.onOpen();
+  };
+
   return (
     <div className="flex flex-col ">
       <div className="flex justify-between items-center px-5 pt-4">
@@ -12,6 +26,7 @@ const Library = () => {
           <p className="text-neutral-400 font-medium text-md"> Your Library</p>
         </div>
         <AiOutlinePlus
+          onClick={onClick}
           className="text-neutral-400 cursor-pointer hover:text-white transition"
           size={26}
         />
