dd33379abca1b2d276269716ee80d34a541d2abe
ianirudhkhabya <ianirudhkhabya@gmail.com>
2024-01-18 20:58:22 +0530

    Add remotePatterns for Supabase image domains

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    // domains: ["mynirhomqqlmayjfxemr.supabase.co"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mynirhomqqlmayjfxemr.supabase.co",
+        pathname: "**",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
