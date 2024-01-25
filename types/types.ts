1d71e050ad5588511cbf0dbac0ca526d50bd0816
ianirudhkhabya <ianirudhkhabya@gmail.com>
2024-01-25 15:05:18 +0530

    Add Stripe integration for payment processing

--- types/types.ts
+++ types/types.ts
@@ -3,20 +3,10 @@ import Stripe from "stripe";
 export interface Song {
   id: string;
   user_id: string;
-  title: string;
   author: string;
+  title: string;
   song_path: string;
   image_path: string;
-}
-
-export interface UserDetails {
-  id: string;
-  first_name: string;
-  last_name: string;
-  full_name?: string;
-  avatar_url?: string;
-  billing_address?: Stripe.Address;
-  payment_method?: Stripe.PaymentMethod[Stripe.PaymentMethod.Type];
 }
 
 export interface Product {
@@ -24,7 +14,7 @@ export interface Product {
   active?: boolean;
   name?: string;
   description?: string;
-  images?: string;
+  image?: string;
   metadata?: Stripe.Metadata;
 }
 
@@ -41,6 +31,25 @@ export interface Price {
   trial_period_days?: number | null;
   metadata?: Stripe.Metadata;
   products?: Product;
+}
+
+export interface Customer {
+  id: string;
+  stripe_customer_id?: string;
+}
+
+export interface UserDetails {
+  id: string;
+  first_name: string;
+  last_name: string;
+  full_name?: string;
+  avatar_url?: string;
+  billing_address?: Stripe.Address;
+  payment_method?: Stripe.PaymentMethod[Stripe.PaymentMethod.Type];
+}
+
+export interface ProductWithPrice extends Product {
+  prices?: Price[];
 }
 
 export interface Subscription {
