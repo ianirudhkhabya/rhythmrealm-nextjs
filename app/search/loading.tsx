76cb6116f1078a7f0c3f7cda70801fe30d818e87
ianirudhkhabya <ianirudhkhabya@gmail.com>
2024-01-25 16:35:23 +0530

    Update ScaleLoader props

--- app/search/loading.tsx
+++ app/search/loading.tsx
@@ -6,7 +6,13 @@ import Box from "@/components/Box";
 const Loading = () => {
   return (
     <Box className="h-full flex items-center justify-center">
-      <ScaleLoader color="#22c55e" margin={3} radius={5} width={5} />
+      <ScaleLoader
+        color="#22c55e"
+        margin={3}
+        radius={5}
+        width={5}
+        speedMultiplier={3}
+      />
     </Box>
   );
 };
